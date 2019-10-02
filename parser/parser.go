a0f28a9000a4e4aeeb26bb5140da4c08186cb11b
Parham Alvani <parham.alvani@gmail.com>
2019-10-03 02:25:35 +0330

    feat: Create Parser Package

--- parser/parser.go
+++ parser/parser.go
@@ -11,33 +11,16 @@
  * +===============================================
  */
 
-package main
+package parser
 
 import (
-	"fmt"
 	"io"
-	"os"
 	"strconv"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/sirupsen/logrus"
 )
-
-func main() {
-	f, err := os.Open("creep.html")
-	if err != nil {
-		return
-	}
-	timelines, err := ExtractTimeline(f)
-	if err != nil {
-		return
-	}
-
-	for _, t := range timelines {
-		fmt.Printf("Tweet %d from %s at %s: %s\n", t.Index, t.User, t.At, t.Content)
-	}
-}
 
 // Timeline represents an element on twitter timeline
 type Timeline struct {
