a066c9d4d6601808e1d8e3486f22378d077e421c
Parham Alvani <parham.alvani@gmail.com>
2019-10-03 07:55:49 +0330

    feat: Add Configuration Package

---
+++ config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"bytes"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+// Config holds all configurations
+type Config struct {
+	Target string
+	Mail   struct {
+		Host     string
+		Port     int
+		Username string
+		Password string
+	}
+}
+
+// New reads configuration with viper
+func New() Config {
+	var instance Config
+
+	v := viper.New()
+	v.SetConfigType("yaml")
+	v.AddConfigPath(".")
+	v.SetConfigName("config.default")
+
+	if err := v.ReadConfig(bytes.NewBufferString(Default)); err != nil {
+		logrus.Fatalf("fatal error loading **default** config file: %s \n", err)
+	}
+
+	v.SetConfigName("config")
+
+	if err := v.MergeInConfig(); err != nil {
+		logrus.Warnf("no config file found, using defaults and environment variables")
+	}
+
+	v.SetEnvPrefix("loser_scraper")
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
+	v.AutomaticEnv()
+
+	if err := v.UnmarshalExact(&instance); err != nil {
+		logrus.Fatalf("unmarshaling error: %s", err)
+	}
+	logrus.Infof("following configuration is loaded:\n%+v", instance)
+
+	return instance
+}
---
+++ config/default.go
@@ -0,0 +1,11 @@
+package config
+
+// Default represents default configuration in YAML format with 2-space
+const Default = `
+target: 1995parham
+mail:
+  host: smtp.gmail.com
+  port: 465
+  username: me
+  password: secret
+`
