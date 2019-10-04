e2e38ba2b55fa3691ba18a846a2f8f7e8a6071df
Parham Alvani <parham.alvani@gmail.com>
2019-10-04 09:32:36 +0330

    feat: Periodically Scrap Twitter Page

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/1995parham/loser-scraper/config"
 	"github.com/1995parham/loser-scraper/parser"
 	"github.com/1995parham/loser-scraper/scrap"
+	"github.com/robfig/cron/v3"
 	"github.com/sirupsen/logrus"
 )
 
@@ -14,23 +18,40 @@ func main() {
 
 	cfg := config.New()
 
-	sc := scrap.New(cfg.Target)
-	rd, err := sc.Scrap()
-	if err != nil {
-		logrus.Fatalf("scrap failed: %s", err)
-		return
-	}
-	defer func() {
-		if err := rd.Close(); err != nil {
-			logrus.Errorf("reader closed: %s", err)
+	c := cron.New()
+
+	_, err := c.AddFunc(fmt.Sprintf("@every %s", cfg.Period), func() {
+		sc := scrap.New(cfg.Target)
+		rd, err := sc.Scrap()
+		if err != nil {
+			logrus.Fatalf("scrap failed: %s", err)
+			return
 		}
-	}()
-	ts, err := parser.ExtractTimeline(rd)
+		defer func() {
+			if err := rd.Close(); err != nil {
+				logrus.Errorf("reader closed: %s", err)
+			}
+		}()
+		ts, err := parser.ExtractTimeline(rd)
+		if err != nil {
+			logrus.Errorf("parser failed: %s", err)
+			return
+		}
+		for _, t := range ts {
+			logrus.Infof("Tweet %d from %s at %s: %s\n", t.Index, t.User, t.At, t.Content)
+		}
+	})
 	if err != nil {
-		logrus.Fatalf("parser failed: %s", err)
-		return
+		logrus.Fatalf("failed to register scraper: %s", err)
 	}
-	for _, t := range ts {
-		fmt.Printf("Tweet %d from %s at %s: %s\n", t.Index, t.User, t.At, t.Content)
-	}
+
+	c.Start()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	fmt.Println("18.20 As always ... left me alone")
+
+	c.Stop()
 }
